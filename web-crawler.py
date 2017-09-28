670ddb8b35131b6acd240a286ef759cfcd94133e
Hoang Long <litpuvn@gmail.com>
2017-09-28 15:49:45 -0500

    update web crawler and config

--- web-crawler.py
+++ web-crawler.py
@@ -1,5 +1,7 @@
 import yaml
 import os
+import requests
+
 
 config = os.path.join(os.path.dirname(__file__), "web-crawler-config.yaml")
 
@@ -7,4 +9,23 @@ sites = []
 with open(config, "r") as f:
     sites = yaml.load(f)
 
-print("done")
+for site in sites:
+    for test in site["tests"]:
+        s = requests.Session()
+        url = test["url"]
+        method = test["method"]
+
+        print("*** Test Set:", test["name"], "***")
+        for case in test["cases"]:
+            print("Test case:", case["name"])
+
+            params = case["params"]
+            expectedOutput = case["output"]
+
+            if method.lower() == "post":
+                r = s.post(url, data=params)
+                if r.status_code != expectedOutput["status_code"]:
+                    print("TEST CASE FAILED")
+                else:
+                    print("TEST CASE PASSED")
+
