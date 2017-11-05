802304910b173f5c3edc9faacc166b344a2c6de4
Hoang Long <litpuvn@gmail.com>
2017-11-05 15:38:26 -0600

    adding test bank

---
+++ test-bank-fetcher.py
@@ -0,0 +1,12 @@
+import yaml
+import os
+import requests
+
+
+input_bank = os.path.join(os.path.dirname(__file__), "test-bank.yaml")
+
+banks = []
+with open(input_bank, "r") as f:
+    banks = yaml.load(f)
+
+print(banks)
