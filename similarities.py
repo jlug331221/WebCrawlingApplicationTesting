8d25ded092a86b20596f746bb9024b45fcc5eb3a
Hoang Long <litpuvn@gmail.com>
2017-11-28 23:13:09 -0600

    add predict method using similarity measure

--- similarities.py
+++ similarities.py
@@ -1,4 +1,5 @@
 from sklearn.metrics.pairwise import cosine_similarity
+from gensim import matutils as mu
 
 '''
 #
@@ -7,3 +8,23 @@ from sklearn.metrics.pairwise import cosine_similarity
 '''
 def find_similarities(vector, feature_vectors):
   return cosine_similarity(vector, feature_vectors)
+
+
+# predict label vector from labeld vectors using cosine similariy
+#
+# vector: vector to be predicted
+# labeld_vectors: is a set of vectors that have been labeled for similarity comparision
+
+def predict(vector, labeled_vectors):
+
+  max = -2
+  most_similarity = None
+
+  for v in labeled_vectors:
+
+    distance = mu.cossim(vector, v)
+    if distance > max:
+      max = distance
+      most_similarity = v
+
+  return most_similarity
