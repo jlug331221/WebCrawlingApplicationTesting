4cc58d7848b4ff23006740059c6e5030dfed22ea
Hoang Long <litpuvn@gmail.com>
2017-11-30 00:49:01 -0600

    add lda prediction similarity

--- similarities.py
+++ similarities.py
@@ -1,5 +1,5 @@
 from sklearn.metrics.pairwise import cosine_similarity
-
+from gensim import corpora, models, similarities
 '''
 #
 # Returns the cosine similarity between vector and feature_vectors.
@@ -7,3 +7,31 @@ from sklearn.metrics.pairwise import cosine_similarity
 '''
 def find_similarities(vector, feature_vectors):
   return cosine_similarity(vector, feature_vectors)
+
+# feature_vector = ['text1', 'texxt2', 'text3']
+def predict_LDA(feature_vector):
+
+  base_saved_output = 'training_set_lda_model'
+
+  dictionary = corpora.Dictionary.load_from_text(base_saved_output + '/training_set_dictionary.txt')
+  corpus = corpora.MmCorpus(base_saved_output + '/training_corpus.mm')
+
+  model_lda = models.LdaModel.load(base_saved_output + '/LDA.model')
+
+  vec_bow = dictionary.doc2bow(feature_vector)
+  vec_lda = model_lda[vec_bow]
+
+  index = similarities.MatrixSimilarity(model_lda[corpus])
+  # compare similarity between vec_lda and the rest of the corpus
+  sims = index[vec_lda]
+  # sort the sims descending order
+  sims = sorted(enumerate(sims), key=lambda item: -item[1])
+
+  most_similar = sims[0]
+
+  print(most_similar)
+
+
+
+
+predict_LDA( ['email', 'text', 'email', 'email', '35'])
